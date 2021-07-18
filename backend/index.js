98c58ae7597426d72496d9d0cc41a1d2ac334d37
estiagasi <69905901+estiagasi@users.noreply.github.com>
2021-07-18 22:00:59 +0300

    create project

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+
+const express = require('express');
+const app = express(),
+      bodyParser = require("body-parser");
+      port = 4000;
+
+const fs = require('fs');
+
+let rawdata = fs.readFileSync('data.json');
+let notes = JSON.parse(rawdata);
+
+app.use(bodyParser.json());
+
+app.get('/', (req,res) => {
+  res.send('App Works !!!!');
+});
+
+app.get('/api/notes', (req, res) => {
+  console.log('api/noets called!!!!')
+  res.json(notes);
+});
+
+app.post('/api/addnote1', (req, res) => {
+  const note = req.body.note;
+  console.log('Adding note:', note);
+  notes.push(note);
+  fs.writeFileSync('data.json', JSON.stringify(notes,null, 2));
+  res.json("noet addedd");
+});
+
+app.put('/api/editnote', (req, res) => {
+  const newnote = req.body.note;
+  notes.forEach((note, index) => {
+      if (note.id === newnote.id) {
+        notes[index].title = newnote.title
+        notes[index].body = newnote.body
+      }
+    });
+  fs.writeFileSync('data.json', JSON.stringify(notes,null, 2));
+  res.json("noet editeddd");
+});
+
+app.delete('/api/deleteNote/:id', (req, res) => {
+ const noteId = req.params.id;
+  notes.forEach((note, index) => {
+    console.log(note.body)
+    if (note.id == noteId) {
+      notes.splice(index, 1);
+    }
+  });
+  // notes = notes.filter((note) => { return note.id != noteId });
+  console.log('delete user::::::::', noteId);
+  res.json("noet deleted");
+  fs.writeFileSync('data.json', JSON.stringify(notes, null, 2));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server listening on the port::${port}`);
+});
